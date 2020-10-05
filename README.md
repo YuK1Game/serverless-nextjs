8153b492536ada87b68398f6f967b9090e518a7f
YuK1Game <yuk1.wcat@gmail.com>
2020-10-05 16:17:32 +0900

    wip

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+`sls deploy --env=development`
+`sls deploy --env=production`
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,23 +1,18 @@
 import Head from 'next/head'
-import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Home</title>
+        <title>Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        HOME?
-        <Link href="/blog">
-          <a>HELLO</a>
-        </Link>
+        Blog
       </main>
 
-      <footer>
-      </footer>
+      <footer></footer>
     </div>
   )
 }
